36b8c64b879c9a097724393ba325689aac77d65d
william-fritz <splizert@gmail.com>
2020-06-07 18:58:25 +0700

    added devise and added basic sign-in and log-out on products

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :products
-  root to: "product#index"
+  root to: "products#index"
 end
